d4864968390f5c2cd17c9ac3039072dc1f6f2432
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2023-11-03 10:34:05 +0000

    Edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Amirali Feyzbakhsh",
+  title: "Animal Biologist & Programmer",
+  email: "feyzbakhsh.amirali@gmail.com",
+  gitHub: "AAFBuniversityGit",
+  instagram: "amir_.alifb",
+  linkedIn: "amirali-feyzbakhsh",
 };
 
 const primaryColor = "#4E567E";
